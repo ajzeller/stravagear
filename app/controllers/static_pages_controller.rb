0732bae4b9aebd2ead8321a45cb2b3f1d080ce96
Andrew <andrew.j.zeller@gmail.com>
2017-03-25 19:45:50 -0500

    Added index, about, dashboard pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+  def dashboard
+  end
+
+  def profile
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'static_pages#index'
+  get 'static_pages/index'
+  get 'static_pages/about'
+  get 'static_pages/dashboard'
+  get 'static_pages/profile'
+  get '/about', to: 'static_pages#about'
+  get '/dashboard', to: 'static_pages#dashboard'
+  get '/profile', to: 'static_pages#profile'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
